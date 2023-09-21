b60ebf380ffa8791cc265a0dba129c2bf6809e88
hwiram <djmjy2@naver.com>
2023-09-21 16:09:17 +0900

    init commit input&output

---
+++ salary/sal_input.py
@@ -0,0 +1,12 @@
+# input the info
+# sal_no / level/ ho/ sudang/ payment/ tax / tot_payment / diff(조정액)
+
+def myinput(employee) :
+    sal_no = input("사원번호 : ")
+    sal_level = input("급수 : ")
+    sal_ho = input("호 : ")
+    sal_sudang = input("수당 : ")
+    employee['sal_no'] = sal_no
+    employee['sal_level'] = sal_level
+    employee['sal_ho'] = sal_ho
+    employee['sal_sudang'] = sal_sudang
